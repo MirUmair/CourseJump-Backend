077b8c03b086a22011fcf5ed319acf10a6e94c4d
MirUmair <69842809+MirUmair@users.noreply.github.com>
2024-10-10 20:00:27 +0500

    video functionality added

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -10,13 +10,9 @@ const router = express.Router();
 
 // Define routes
 router.post('/create', upload.single('courseImage'), createCourse);
-
 router.get('/', getAllCourses);                    // GET all courses
-router.get('/:id', getCourseById);                 // GET a course by ID
-// router.post('/', createCourse);                    // POST a new course
-// router.put('/:id', updateCourse);                  // PUT (update) a course by ID
+router.get('/:id', getCourseById);                        // PUT (update) a course by ID
 router.put('/update/:id', upload.single('courseImage'), updateCourse);
-
 router.delete('/:id', deleteCourse);               // DELETE a course by ID
 router.get('/user/token', getCoursesByUser);
 
